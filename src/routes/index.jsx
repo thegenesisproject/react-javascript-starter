be2becf54378dc8b81bbfe0fb1b45ddac6c88faa
Robin Hood <robinhood.experiment@gmail.com>
2020-10-23 15:41:32 +0200

    bug(eslint): fix errors in Hound CI
    - https://egghead.io/lessons/react-linting-react-jsx-with-eslint-in-es6
    - https://stackoverflow.com/a/43426331/2661028
    - https://babeljs.io/blog/2020/07/13/the-state-of-babel-eslint

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 // Layouts
 import DefaultLayout from '../layouts/DefaultLayout';
